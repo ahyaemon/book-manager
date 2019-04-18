322c7af8585878f9174b2b97fed2a725c4b999ac
ahyaemon <iamahyaemon@gmail.com>
2019-04-18 17:35:05 +0900

    vuetify適用

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,38 +1,49 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+<template lang='pug'>
+  v-app
+    v-toolbar(app)
+      v-toolbar-title BOOK MANAGER
+    v-content
+      v-container.container(fluid)
+        v-layout(row wrap)
+          template(v-for="book in books")
+            v-flex(pt-3, xs12)
+              v-card.card
+                v-card-title
+                  h1 {{ book.title }}
+                v-card-text
+                  div {{ book.author }}
+                  div {{ book.publisher }}
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+      v-btn(color='pink', dark, fixed, bottom, right, fab) +
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<script lang='ts'>
+    import { Component, Vue } from 'vue-property-decorator'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+    @Component
+    export default class App extends Vue {
+      private books = [
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: 'プログラミングRust', author: 'Jim Blandy, Jason Orendorff他', publisher: '桃源社' },
+        { title: 'Programming Rust: Fast, Safe Systems Development', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+        { title: '吾輩は猫である', author: '夏目漱石', publisher: '桃源社' },
+      ]
+
+
     }
-  }
-}
 </script>
+
+<style lang='scss'>
+  body {
+    font-family: 游ゴシック, YuGothic, "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, sans-serif;
+  }
+
+  .container {
+    width: 1000px;
+  }
+</style>
