81cae779aec86b3bb1db501cffb6d1858dd6585b
ahyaemon <iamahyaemon@gmail.com>
2019-04-18 11:00:28 +0900

    initial commit

---
+++ src/main/kotlin/com/iijima/bookmanager/web/html/IndexController.kt
@@ -0,0 +1,15 @@
+package com.iijima.bookmanager.web.html
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping
+class IndexController {
+
+    @RequestMapping
+    fun index(): String {
+        return "index.html"
+    }
+
+}
---
+++ src/test/kotlin/com/iijima/bookmanager/BookManagerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.iijima.bookmanager
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BookManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
